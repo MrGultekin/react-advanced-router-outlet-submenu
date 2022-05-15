9baddc8feb5e5c6448d5ec4ca93bf4a6988cd086
MrGultekin <gultekin.de@gmail.com>
2022-05-15 21:57:49 +0200

    Finished.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import './App.scss';
 import { PageWelcome } from './pages/PageWelcome';
 import { PageBooks } from './pages/PageBooks';
-import { PageAbout } from './pages/PageAbout';
-import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 import { PageHowtos } from './pages/PageHowtos';
+import { PageAbout } from './pages/PageAbout';
+import { Howto } from './components/Howto';
+
+import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className="App">
+		<div className='App'>
 			<h1>Personal Site</h1>
 			<hr />
-			<NavLink to="/welcome">Welcome</NavLink> |{' '}
-			<NavLink to="/books">Books</NavLink> |{' '}
-			<NavLink to="/howtos">Howtos</NavLink> |{' '}
-			<NavLink to="/about">About</NavLink>
+			<NavLink to='/welcome'>Welcome</NavLink> |{' '}
+			<NavLink to='/books'>Books</NavLink> |{' '}
+			<NavLink to='/howtos'>Howtos</NavLink> |{' '}
+			<NavLink to='/about'>About</NavLink>
 			<hr />
 			<Routes>
-				<Route path="/welcome" element={<PageWelcome />} />
-				<Route path="/books/*" element={<PageBooks />} />
-				<Route path="/about" element={<PageAbout />} />
-				<Route path="/howtos" element={<PageHowtos />} />
-				<Route path="/" element={<Navigate to="welcome" replace />}/>
+				<Route path='/welcome' element={<PageWelcome />} />
+				<Route path='/books/*' element={<PageBooks />} />
+				<Route path='/howtos' element={<PageHowtos />}>
+					<Route path=':id' element={<Howto />} />
+				</Route>
+				<Route path='/about' element={<PageAbout />} />
+				<Route path='/' element={<Navigate to='/welcome' replace />} />
 			</Routes>
 		</div>
 	);
--- src/pages/PageHowtos.jsx
+++ src/pages/PageHowtos.jsx
@@ -1,14 +1,90 @@
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useState,useEffect} from 'react';
+import {
+	useParams,
+	NavLink,
+	useNavigate,
+	Outlet,
+	useLocation,
+} from 'react-router-dom';
 
 const url = 'https://edwardtanguay.netlify.app/share/howtos.json';
+const currentCategory = 'PHP';
 
 export const PageHowtos = () => {
-	const [howtos,setHowtos] = useState([]);
+	const [howtos, setHowtos] = useState([]);
+	let { id } = useParams();
+	id = Number(id);
+	const navigate = useNavigate();
+	const location = useLocation();
+	console.log(location.pathname);
+
+	useEffect(() => {
+		(async () => {
+			const allHowtos = (await axios.get(url)).data;
+			const _howtos = allHowtos.filter(
+				(m) =>
+					m.category.toLowerCase() === currentCategory.toLowerCase()
+			);
+			setHowtos(_howtos);
+		})();
+	}, []);
+
+	const getCurrentHowto = () => {
+		if (id) {
+			return howtos.find((m) => m.id == id);
+		}
+		// else {
+		// 	if (howtos.length > 0) {
+		// 		const _id = String(howtos[0].id);
+		// 		navigate(_id);
+		// 	}
+		// }
+	};
+
+	const handleCloseButton = () => {
+		navigate('/howtos');
+	};
+
 	return (
-		<>
+		<div className="page_howtos">
 			<h2>Howtos</h2>
-			<p>I currently have {howtos.length} howtos:</p>
-		</>
-	)
-}
+			{howtos.length === 0 ? (
+				<div className="loading">Loading...</div>
+			) : (
+				<>
+					<p>
+						I currently have {howtos.length} {currentCategory}{' '}
+						howtos:
+					</p>
+
+					{location.pathname === '/howtos' ? (
+						<nav>
+							<ul>
+								{howtos.map((howto, index) => {
+									return (
+										<li key={index}>
+											<NavLink to={`${howto.id}`}>
+												{howto.title}
+											</NavLink>
+										</li>
+									);
+								})}
+							</ul>
+						</nav>
+					) : (
+						<div className="buttonArea">
+							<button
+								className="close"
+								onClick={handleCloseButton}
+							>
+								close
+							</button>
+						</div>
+					)}
+					<Outlet context={getCurrentHowto()} />
+				</>
+			)}
+		</div>
+	);
+};
