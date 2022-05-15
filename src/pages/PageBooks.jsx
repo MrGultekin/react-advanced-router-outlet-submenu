3dfd70fabdecba9fbc2d6f225568067376f6a2e7
MrGultekin <gultekin.de@gmail.com>
2022-05-15 15:36:29 +0200

    Books NavLink works.

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,12 +1,19 @@
 import { BookGroupFantasy } from '../components/BookGroupFantasy';
 import { BookGroupPolitics } from '../components/BookGroupPolitics';
 import { BookGroupTechnology } from '../components/BookGroupTechnology';
+import { NavLink } from 'react-router-dom';
 
 export const PageBooks = () => {
 	return (
 		<div className="page_books">
 			<h2>Books</h2>
 			<p>This is the book page.</p>
+			<ul>
+				<li><NavLink to="fantasy">Fantasy</NavLink></li>
+				<li><NavLink to="politics">Politics</NavLink></li>
+				<li><NavLink to="technology">Technology</NavLink></li>
+			</ul>
+
 			<BookGroupFantasy/>
 			<BookGroupPolitics/>
 			<BookGroupTechnology/>
