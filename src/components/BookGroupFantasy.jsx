b83ab1968d0c69a9f4709ca8abdb77834d074d7e
MrGultekin <gultekin.de@gmail.com>
2022-05-15 15:24:00 +0200

    3 components BookGroups are in.

---
+++ src/components/BookGroupFantasy.jsx
@@ -0,0 +1,9 @@
+
+export const BookGroupFantasy = ()=>{
+	return(
+		<>
+		<h3>Fantasy</h3>
+		<p>These are my fantasy books.</p>
+		</>
+	)
+}
---
+++ src/components/BookGroupPolitics.jsx
@@ -0,0 +1,9 @@
+
+export const BookGroupPolitics = ()=>{
+	return(
+		<>
+		<h3>Political Books</h3>
+		<p>Books on society and politics.</p>
+		</>
+	)
+}
